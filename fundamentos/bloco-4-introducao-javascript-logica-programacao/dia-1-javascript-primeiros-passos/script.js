56ffbcea9bece491b24e2ac65f53fa492901b8a5
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 17:05:55 -0300

    Exercicio 7

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -101,3 +101,28 @@ case "peão":
     console.log("Peão: move-se uma casa para frente, exceto no primeiro movimento, quando pode mover-se em duas casas");
     break;
 }
+
+// Exercício 7
+const nota = 85.5
+
+if (nota < 0 || nota > 100) {
+    console.log("Nota incorreta!");
+}
+else if (nota >= 90){
+    console.log("Nota A");
+}
+else if (nota >= 80){
+    console.log("Nota B");
+}
+else if (nota >= 70){
+    console.log("Nota C");
+}
+else if (nota >= 60){
+    console.log("Nota D");
+}
+else if (nota >= 50){
+    console.log("Nota E");
+}
+else {
+    console.log("Nota F")
+}
--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/tempCodeRunnerFile.js
@@ -1,17 +1,23 @@
-let anguloA = 90;
-let anguloB = 45;
-let anguloC = 45;
+const nota = 85.5
 
-let somaAngulos = anguloA + anguloB + anguloC
-let angulosPositivos = anguloA > 0 && anguloB > 0 && anguloC > 0;
-
-if (angulosPositivos) {
-    if (somaAngulos === 180){
-        console.log(true);
-    }
-    else {
-        console.log(false);
-    }
-} else {
-    console.log("Erro ângulo inválido");
+if (nota < 0 || nota > 100) {
+    console.log("Nota incorreta!");
+}
+else if (nota >= 90){
+    console.log("Nota A");
+}
+else if (nota >= 80){
+    console.log("Nota B");
+}
+else if (nota >= 70){
+    console.log("Nota C");
+}
+else if (nota >= 60){
+    console.log("Nota D");
+}
+else if (nota >= 50){
+    console.log("Nota E");
+}
+else {
+    console.log("Nota F")
 }
