f1e59b568b80f90ab711b81232012d6835a336a0
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 20:26:40 -0300

    Exerc 4 e 5

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/tempCodeRunnerFile.js
@@ -1,11 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
-let media = 0;
+let majorNum = 0;
 
-for (let i = 0; i < numbers.length; i += 1) {
-    result = numbers[i];
-    
-}
-media = result / numbers.length;
-console.log(media);
-
+for (index = 0; index < numbers.length; index += 1) {
+    if (majorNum < numbers[index]) {
+        majorNum = numbers[index];
+    } else {
+        majorNum = majorNum;
+    }
+} 
+console.log(majorNum);
