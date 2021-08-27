8db7102f8e40de2dbe313e3d02d99ebbdaf4ad1f
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 20:20:16 -0300

    Exercicio3

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/script.js
@@ -6,10 +6,22 @@ for (let i = 0; i < numbers.length; i += 1) {
 }
 
 // Exercício 2
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sumArray = 0; 
 
 for (let i = 0; i < numbers.length; i += 1) {
     sumArray += numbers[i]; 
 }
  console.log(sumArray);
+
+// Exercício 3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+let media = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    result = numbers[i];
+    
+}
+media = result / numbers.length;
+console.log(media);
+
--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/tempCodeRunnerFile.js
@@ -1,7 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sumArray = 0; 
+let result = 0;
+let media = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
-    sumArray += numbers[i]; 
+    result = numbers[i];
+    
 }
- console.log(sumArray);
+media = result / numbers.length;
+console.log(media);
+
