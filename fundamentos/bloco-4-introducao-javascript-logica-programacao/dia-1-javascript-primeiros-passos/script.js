4e85a46fe22aee54ffb4ec780478634e6dd12595
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 17:22:16 -0300

    Exercicio 9

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -138,3 +138,14 @@ if (A % 2 === 0 || B % 2 === 0 || C % 2 === 0) {
     console.log('false');
 }
 
+// Exercício 9
+const A = 6;
+const B = 4; 
+const C = 222; 
+
+if ((A % 2) !== 0 || (B % 2) !== 0 || (C % 2) !== 0){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/tempCodeRunnerFile.js
@@ -1,9 +1,10 @@
-const A = 31;
-const B = 67;
-const C = -993;
+const A = 6;
+const B = 4; 
+const C = 222; 
 
-if (A % 2 === 0 || B % 2 === 0 || C % 2 === 0) {
-    console.log('true');
-} else {
-    console.log('false');
+if ((A % 2) !== 0 || (B % 2) !== 0 || (C % 2) !== 0){
+    console.log(true);
+}
+else {
+    console.log(false);
 }
