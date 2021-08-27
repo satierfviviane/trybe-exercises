337665de6ed2b78ea380375356556213b0be0d09
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 17:12:49 -0300

    Exercicio 8

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -126,3 +126,15 @@ else if (nota >= 50){
 else {
     console.log("Nota F")
 }
+
+// Exercício 8
+const A = 31;
+const B = 67;
+const C = -993;
+
+if (A % 2 === 0 || B % 2 === 0 || C % 2 === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passos/tempCodeRunnerFile.js
@@ -1,23 +1,9 @@
-const nota = 85.5
+const A = 31;
+const B = 67;
+const C = -993;
 
-if (nota < 0 || nota > 100) {
-    console.log("Nota incorreta!");
-}
-else if (nota >= 90){
-    console.log("Nota A");
-}
-else if (nota >= 80){
-    console.log("Nota B");
-}
-else if (nota >= 70){
-    console.log("Nota C");
-}
-else if (nota >= 60){
-    console.log("Nota D");
-}
-else if (nota >= 50){
-    console.log("Nota E");
-}
-else {
-    console.log("Nota F")
+if (A % 2 === 0 || B % 2 === 0 || C % 2 === 0) {
+    console.log('true');
+} else {
+    console.log('false');
 }
