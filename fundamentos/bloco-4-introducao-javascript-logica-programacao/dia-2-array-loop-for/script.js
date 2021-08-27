a4f661d9dec59db2c115c7a91e1503b971770bc2
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 20:14:55 -0300

    Exercicio 1 e 2

---
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/script.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercício 1
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+}
+
+// Exercício 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumArray = 0; 
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sumArray += numbers[i]; 
+}
+ console.log(sumArray);
