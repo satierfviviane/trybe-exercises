0ceb1894333c4296d97982bb4cfb4fa98917831b
Viviane Freitas <satierfviviane@gmail.com>
2021-08-27 20:22:30 -0300

    Exercicio 5

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-2-array-loop-for/script.js
@@ -25,3 +25,19 @@ for (let i = 0; i < numbers.length; i += 1) {
 media = result / numbers.length;
 console.log(media);
 
+// Exercicio 4 e 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+let media = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    result = numbers[i];
+    
+}
+media = result / numbers.length;
+
+if (media > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor que 20');
+}
