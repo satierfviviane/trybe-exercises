bffbed4399c8e2201f866d8c158c4cf7dee63d69
Viviane Freitas <satierfviviane@gmail.com>
2021-08-29 02:40:55 -0300

    Exercicios 1 - Objetos e For In

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-4-objetos-funcoes/exercicios-1-obj-forin.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-4-objetos-funcoes/exercicios-1-obj-forin.js
@@ -2,6 +2,30 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+// Exercicio 1
+console.log('Bem-vinda ' + info.personagem);
+
+// Exercicio 2
+info.recorrente = 'Sim';
+console.log(info);
+
+// Exercicio 3
+for (let index in info) {
+    console.log(index);
+}
+
+// Exercicio 4
+for (let index in info) {
+    console.log(info[index]);
+}
+
+// Exercicio 5
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
 };
 
