3e330a5a48a48719d7c0a17e097a464c9b6dca9d
Viviane Freitas <satierfviviane@gmail.com>
2021-09-03 03:34:19 -0300

    Iniciando Estilizacao

--- front-end/bloco-11-introducao-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.js
+++ front-end/bloco-11-introducao-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.js
@@ -6,7 +6,7 @@ class Pokedex extends React.Component {
   render() {
     const { pokemons } = this.props;  
     return (
-      <main>
+      <main className="pokedex">
         {pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
       </main>
     );
--- front-end/bloco-11-introducao-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.js
+++ front-end/bloco-11-introducao-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
-
-class Pokemon extends React.Component{
-    render () {
-        const { pokemon } = this.props;
-        return (
-            <section className="pokemon-card">
-                <h1>{ pokemon.name }</h1>
-                <p>{ pokemon.type }</p>
-                <p>{`Average Weight ${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`}</p>
-                <img src={ pokemon.image } alt={ pokemon.name } />  
-
-            </section>
-        );
-    }
+class Pokemon extends React.Component {
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <div className="pokemon">
+        <div>
+          <h2>{pokemon.name}</h2>
+          <p>{pokemon.type}</p>
+          <p>{`Average Weight ${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`}</p>
+          <img src={pokemon.image} alt={pokemon.name} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pokemon;
-
-
